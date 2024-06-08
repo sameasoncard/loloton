cc05aefef29096a54ad30fce3b25e9348f12f587
sameasoncard <ooonakolenke@gmail.com>
2024-06-08 17:44:02 +0300

    fix: change vite cfg

--- frontend/lottery-tma/vite.config.ts
+++ frontend/lottery-tma/vite.config.ts
@@ -9,7 +9,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/loloton/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
