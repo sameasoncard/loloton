9e8823526f7d936d9ae95210402200fa59ffa41d
sameasoncard <ooonakolenke@gmail.com>
2024-06-08 17:34:45 +0300

    feat: init frontend
    Start development in frontend for TMA

---
+++ frontend/lottery-tma/src/pages/IndexPage/IndexPage.tsx
@@ -0,0 +1,36 @@
+import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import type { FC } from 'react';
+
+import { Link } from '@/components/Link/Link.tsx';
+
+import tonSvg from './ton.svg';
+
+import './IndexPage.css';
+
+export const IndexPage: FC = () => {
+  return (
+    <List>
+      <Section
+        header='Features'
+        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
+      >
+        <Link to='/ton-connect'>
+          <Cell
+            before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
+            subtitle='Connect your TON wallet'
+          >
+            TON Connect
+          </Cell>
+        </Link>
+      </Section>
+      <Section
+        header='Application Launch Data'
+        footer='These pages help developer to learn more about current launch information'
+      >
+        <Link to='/init-data'>
+          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
+        </Link>
+      </Section>
+    </List>
+  );
+};
